521c9deb49e3a5b29d504f25f1d501858f9ba105
emmanueladekunle <emmanuels.adekunle@gmail.com>
2025-05-31 13:21:22 +0100

    package installation/error checks+correct importstatement used

--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -14,7 +14,7 @@ router.post("/user-registration", userRegistration);
 router.post("/verify-user", verifyUser);
 router.post("/login-user", loginUser);
 router.post("/forgot-password-user", userForgotPassword);
-router.post("/freset-password-user", resetUserPassword);
+router.post("/reset-password-user", resetUserPassword);
 router.post("/verify-forgot-password-user", verifyUserForgotPassword);
 
 export default router;
