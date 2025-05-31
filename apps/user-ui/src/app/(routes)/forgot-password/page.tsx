6014b81ebc9f9f960e8520016ab1cc95b161fcfd
emmanueladekunle <emmanuels.adekunle@gmail.com>
2025-05-31 11:06:24 +0100

    I have to now reorganize the ForgotPassword logic into dedicated flow to split out the forgot password logic from the previous login implementation I had. This change is to improve code separation and also maintains cleaner UX. I Created a dedicated ForgotPassword component to focus solely on email input and future OTP handling. Then I started laying the foundation for OTP verification flow by including input states and timer logic. I will now retain `startResendTimer` and reusable state mechanisms for later integration with signup flow.

--- apps/user-ui/src/app/(routes)/forgot-password/page.tsx
+++ apps/user-ui/src/app/(routes)/forgot-password/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import GoogleButton from "../../shared/components/google-button";
 import { Eye, EyeOff } from "lucide-react";
@@ -14,10 +14,14 @@ type FormData = {
   password: string;
 };
 
-const Login = () => {
-  const [passwordVisible, setPasswordVisible] = useState(false);
+const ForgotPassword = () => {
+  const [step, setStep] = useState<"email" | "otp" | "reset">("email");
+  const [otp, setOtp] = useState(["", "", "", ""]);
+  const [userEmail, setUserEmail] = useState<string | null>(null);
+  const [canResend, setCanResend] = useState(true);
+  const [timer, setTimer] = useState(60);
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
-  const [rememberMe, setRememberMe] = useState(false);
   const router = useRouter();
 
   const {
@@ -27,36 +31,57 @@ const Login = () => {
   } = useForm<FormData>();
 
 
-  const loginMutation = useMutation ({
-    mutationFn: async(data:FormData) => {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URI}/api/login-user`,
-        data,
-        {withCredentials: true }
+  const startResendTimer = () => {
+    setCanResend(false);
+    setTimer(60);
+    const interval = setInterval(() => {
+      setTimer((prev) => {
+        if (prev <= 1) {
+          clearInterval(interval);
+          setCanResend(true);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+  };
+
+
+  const requestOtpMutation = useMutation({
+    mutationFn: async ({ email }: { email: string }) => {
+      const reponse = await axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER_URI}/api/forgot-password-user`,
+        { email }
       );
-      return response.data;
+      return reponse.data;
     },
-    onSuccess: (data) =>{
+    onSuccess: (_, { email }) => {
+      setUserEmail(email);
+      setStep("otp");
       setServerError(null);
-      router.push("/");
+      setCanResend(false);
+      startResendTimer();
     },
-    onError: (error: AxiosError) =>{
-      const errorMessage = (error.response?.data as {message?: string}) ?.message ||
-      "Invalid credentials"
+    onError: (error: AxiosError) => {
+      const errorMessage =
+        (error.response?.data as { message?: string })?.message ||
+        "Invalid OTP. Try again!";
       setServerError(errorMessage);
     },
   });
 
+
   const onSubmit = (data: FormData) => {
-    loginMutation.mutate(data);
+    console.log(data);
   };
 
   return (
     <div className="w-full py-10 min-h-[85vh] bg-[#f1f1f1]">
       <h1 className="text-4xl font-poppins font-semibold text-black text-center">
-        Login
+        Forgot Password
       </h1>
       <p className="text-center text-lg font-medium py-3 text-[#000000099]">
-        Home . Login
+        Home . Forgot-Password
       </p>
 
       <div className="w-full flex justify-center">
@@ -65,19 +90,13 @@ const Login = () => {
             Login to SHOPLINK
           </h3>
           <p className="text-center text-gray-500 mb-4">
-            New User?{' '}
-            <Link href={"/signup"} className= "text-blue-500">
-            Sign Up
+            Return to?{' '}
+            <Link href={"/login"} className= "text-blue-500">
+            Login
             </Link>
 
           </p>
 
-          <GoogleButton />
-          <div className="flex items-center my-5 text-gray-400 text-sm">
-            <div className="flex-1 border-t border-gray-300"/>
-            <span className="px-3">or Sign in with Email</span>
-            <div className="flex-1 border-t border-gray-300"/>
-          </div>
 
           <form onSubmit={handleSubmit(onSubmit)}>
             <label className="block text-gray-700 mb-1">Email</label>
@@ -98,48 +117,13 @@ const Login = () => {
                 {String(errors.email.message)}
               </p>
             )}
-            <label className="block text-gray-700 mb-1">Password</label>
-            <div className="relative">
-              <input
-                type={passwordVisible ? "text" : "password"}
-                placeholder="Min. 6 characters"
-                className="w-full p-2 border border-gray-300 outline-0 !rounded mb-1"
-                {...register ("password",{
-                  required: "Password is required",
-                  minLength: {
-                    value:6,
-                    message: "Password must be at least 6 characters",
-                  },
-                })}
-                />
-                <button type="button" onClick={() => setPasswordVisible(!passwordVisible)}
-                  className="absolute inset-y-0 right-3 flex items-center text-gray-400"
-                >
-                  {passwordVisible? <Eye/> : <EyeOff />}
-                </button> 
-                {errors.password && (
-                  <p className="text-red-500 text-sm">
-                    {String(errors.password.message)}
-                  </p>
-                )}
-            </div>
 
-            <div className="flex justify-between items-center my-4">
-                  <label className="flex items-center text-gray-600">
-                    <input type="checkbox" className="mr-2" checked={rememberMe} onChange={() => setRememberMe(!rememberMe)} 
-                    />
-                    Remember me
-                  </label>
-                  <Link href={"/forgot-password"} className="text-blue-500 text-sm">
-                  Forgot Password?
-                  </Link>
-                </div>
                 <button 
                   type="submit"
                   disabled={loginMutation.isPending}
-                  className="w-full text-lg cursor-pointer bg-black text-white py-2 rounded-lg"
+                  className="w-full text-lg cursor-pointer mt-4 bg-black text-white py-2 rounded-lg"
                 >
-                  {loginMutation?.isPending ? "Logging in ..." : "Login"}
+                  Submit
                 </button>
 
                 {serverError && (
@@ -152,4 +136,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default ForgotPassword;
