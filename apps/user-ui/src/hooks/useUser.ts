f67bdc688eb40a4fe396de54b2d1a0c658f858cf
emmanueladekunle <emmanuels.adekunle@gmail.com>
2025-06-01 01:09:53 +0100

    The implementation of fetchUser function to retrieve the current authenticated user's data with the use of axiosInstance.get("/api/logged-in-user") to send a GET request to the backend. The feed back will be a return of the response.data.user for easy access to the user object.

--- apps/user-ui/src/hooks/useUser.ts
+++ apps/user-ui/src/hooks/useUser.ts
@@ -1,2 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "../utils/axiosInstance";
+
+// fetch user data from API
+const fetchUser = async () => {
+  const response = await axiosInstance.get("/api/logged-in-user");
+  return response.data.user;
+};
+
